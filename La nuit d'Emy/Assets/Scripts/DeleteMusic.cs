2ac67caeb0b972bfd930d7dd037744c2202b9a9f
SPbang <raphbend@live.fr>
2020-01-26 22:39:58 +0100

    Fixed looping music Changed music implementation

---
+++ La nuit d'Emy/Assets/Scripts/DeleteMusic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteMusic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
+
+        foreach (GameObject e in objs)
+        {
+            Destroy(e);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- La nuit d'Emy/Assets/Scripts/Music.cs
+++ La nuit d'Emy/Assets/Scripts/Music.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class Music : MonoBehaviour
 {
     public AudioSource AS;
+    public AudioClip song;
+    float loop_time = 44.0f;
+    float loop_duration = 32.0f;
+    bool switched = false;
     // Start is called before the first frame update
+    // Checking if going over Sample where looping occurs each frame
     void Update()
     {
-        
+        if (AS.timeSamples >= loop_time * song.frequency)
+        {
+            AS.timeSamples -= Mathf.RoundToInt(loop_duration * song.frequency);
+        }
     }
 
     // Update is called once per frame
     void Start()
     {
         AS = GetComponent<AudioSource>();
-        AS.PlayDelayed( 12.0f);
+        song = AS.clip;
+        AS.Play();
     }
 }
